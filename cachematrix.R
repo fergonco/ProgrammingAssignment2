cc378ce1034023e9b52427dc48dfd48d2d92620a
Fernando González <fernando.gonzalez@geomati.co>
2014-10-18 22:15:31 +0200

    Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function builds a matrix object. More info here: https://class.coursera.org/rprog-008/human_grading/view/courses/972581/assessments/3/submissions
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = matrix()) {
+  inverseCache <- NULL
+  setMatrix <- function(newMatrix) {
+    matrix <<- newMatrix
+    inverseCache <<- NULL
+  }
+  getMatrix <- function() matrix
+  setCache <- function(inverse) inverseCache <<- inverse
+  getCache <- function() inverseCache
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setCache = setCache,
+       getCache = getCache)
+  
 }
 
 
-## Write a short comment describing this function
+## Caches the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  cache <- x$getCache()
+  if(!is.null(cache)) {
+    message("getting cached data")
+    return(cache)
+  }
+  matrix <- x$getMatrix()
+  inverse <- solve(matrix, ...)
+  x$setCache(inverse)
+  inverse
 }
